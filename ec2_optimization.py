b987d6a2b00de17f238d516fd74b884ac3658e59
Pulkit Dheer <pulkitkdheer@gmail.com>
2024-10-28 13:15:37 +0530

    orphaned sg deletion code added

--- ec2_optimization.py
+++ ec2_optimization.py
@@ -95,32 +95,70 @@ class EC2Optimizer:
                         logging.info(f"EBS snapshot {snapshot_id} is deleted as the associated volume was not found.")
                 
 
-    def optimize_ec2_instances(self):
+    def optimize_ec2_instances(self, region):
         """Optimize EC2 instances based on their CPU utilization."""
-        regions = self.get_regions()
-        for region in regions:
-            regional_ec2_client = boto3.client('ec2', region_name=region)
-            logging.info(f"Checking instances in region: {region}")
+        ec2 = boto3.client('ec2', region_name=region)
+        logging.info(f"Checking instances in region: {region}")
 
-            all_ec2_data = regional_ec2_client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        all_ec2_data = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
-            for reservation in all_ec2_data['Reservations']:
-                for instance in reservation['Instances']:
-                    instance_id = instance['InstanceId']
-                    if 'Tags' in instance:
-                        for tag in instance['Tags']:
-                            if tag['Key'] == 'Environment' and tag['Value'] in ['staging', 'dev']:
-                                logging.info(f"Instance ID: {instance_id} has the following tags: {tag['Value']}")
-                                datapoints = self.get_compute_usage(instance_id, region)
-                                self.analyze_cpu_utilization(datapoints, instance_id, region)
+        for reservation in all_ec2_data['Reservations']:
+            for instance in reservation['Instances']:
+                instance_id = instance['InstanceId']
+                if 'Tags' in instance:
+                    for tag in instance['Tags']:
+                        if tag['Key'] == 'Environment' and tag['Value'] in ['staging', 'dev']:
+                            logging.info(f"Instance ID: {instance_id} has the following tags: {tag['Value']}")
+                            datapoints = self.get_compute_usage(instance_id, region)
+                            self.analyze_cpu_utilization(datapoints, instance_id, region)
 
-                    else:
-                        logging.info(f"Instance ID: {instance_id} is not in staging or dev.")
+                else:
+                    logging.info(f"Instance ID: {instance_id} is not in staging or dev.")
 
-            self.snapshot_optimizer(region)
 
+
+    def delete_orphaned_sgs(self, orphaned_sg_ids, region):
+        """Deleting all Orphaned Security Groups"""
+        ec2 = boto3.client('ec2', region_name=region)
+
+        for sg_id in orphaned_sg_ids:
+            try:
+                ec2.delete_security_group(GroupId=sg_id)
+                logging.info(f"Orphaned Security Group: {sg_id} in {region} is Deleted.")
+            except Exception as e:
+                logging.inof(f"Failed to delete Security Group: {sg_id} in {region}. Error {str(e)}")
+
+
+    def get_security_groups(self, region):
+        """Listing all the orphaned security gruops"""
+        ec2 = boto3.client('ec2', region_name=region)
+        all_sgs = ec2.describe_security_groups()
+
+        all_sgs_set = set()
+        default_sg_set = set()
+        for sg in all_sgs['SecurityGroups']:
+            sg_id = sg['GroupId']
+            all_sgs_set.add(sg_id)
+            sg_name = sg.get('GroupName', 'default')
+
+            if sg_name == 'default':
+                default_sg_set.add(sg_id)
+
+        instances = ec2.describe_instances()
+        associated_sg_ids = set()
+        for reservation in instances['Reservations']:
+            for instance in reservation['Instances']:
+                for sg in instance.get('SecurityGroups', []):
+                    associated_sg_ids.add(sg['GroupId'])
+
+
+        orphaned_sg_ids = all_sgs_set - associated_sg_ids - default_sg_set
+        self.delete_orphaned_sgs(orphaned_sg_ids, region)
 
 if __name__ == "__main__":
     optimizer = EC2Optimizer()
-    optimizer.optimize_ec2_instances()
-
+    regions = optimizer.get_regions()
+    for region in regions:
+        # optimizer.snapshot_optimizer(region)
+        # optimizer.optimize_ec2_instances(region)
+        optimizer.get_security_groups(region)
